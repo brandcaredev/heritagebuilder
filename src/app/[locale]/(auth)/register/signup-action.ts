3a7a7ef3851d69c94e8504a2eab49b565e4f4e8b
csisrobi <csisrobi@yahoo.com>
2024-12-11 12:29:04 +0200

    signup fix

--- src/app/[locale]/(auth)/register/signup-action.ts
+++ src/app/[locale]/(auth)/register/signup-action.ts
@@ -1,7 +1,19 @@
 "use server";
 import { createClient } from "@/supabase/server";
 import { signUpSchema } from "./signup-schema";
-import { headers } from "next/headers";
+
+const getURL = () => {
+  // TODO URLS
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
 
 export async function signUpWithEmail(data: FormData) {
   const formData = Object.fromEntries(data);
@@ -15,15 +27,11 @@ export async function signUpWithEmail(data: FormData) {
   }
   const supabase = await createClient();
 
-  const headersList = headers();
-  const host = headersList.get("X-Forwarded-Host");
-  const proto = headersList.get("X-Forwarded-Proto");
-
   const { error } = await supabase.auth.signUp({
     email: parsedData.email,
     password: parsedData.password,
     options: {
-      emailRedirectTo: `${proto}://${host}/`,
+      emailRedirectTo: getURL(),
     },
   });
 
